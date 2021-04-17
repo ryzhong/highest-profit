7503c397c8de62014753660b7a9b7a7cb5bdfd8a
ryzhong <ryzhong92@gmail.com>
2021-04-16 17:21:53 -0700

    -Created function to take a file and increase counters -Prints total counter -Prints counter with numerical profit

---
+++ highestProfit.js
@@ -0,0 +1,27 @@
+const csv = require('csv-parser');
+const fs = require('fs');
+
+module.exports = (file) => {
+    //An array to hold JSON formatted data
+    let newData = [];
+    //holds counters
+    let totalCounter = 0;
+    let newCounter = 0;
+
+    fs.createReadStream(file)
+        .pipe(csv()) //uses csv-parser to convert data into JSON format
+        .on('data', row => { //counts total rows in CSV file
+            totalCounter++;
+        })
+        .on('data', row => {    //Adds Obj to arr and skips over profits that are non-number 
+            if (!isNaN(+row['Profit (in millions)'])) {
+                newData.push(row)
+                newCounter++;
+            }
+        })
+        .on('end', () => {
+            //logging total CSV rows, CSV rows with numbers in profit
+            console.log(totalCounter)
+            console.log(newCounter)
+        });
+}
